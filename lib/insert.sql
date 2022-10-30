50fb63b2f827e0956ba9aa2a3ce496e4abd03762
Benson <benson.odongo@student.moringaschool.com>
2022-10-30 16:42:04 +0300

    Completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Maru', 3, 'male', 'Brown', 'mingle', 'yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('king', 5, 'female', 'Brown', 'mingle', 'yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Max', 6, 'male', 'Brown', 'mingle', 'yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mark', 4, 'female', 'Brown', 'mingle', 'yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Nick', 7, 'male', 'Brown', 'mingle', 'yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mike', 8, 'male', 'Brown', 'mingle', 'yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mash', 9, 'female', 'Brown', 'mingle', 'yes');
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 3, 'male', 'Brown', 'mingle', 'yes');
